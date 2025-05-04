c34d1a6f857ef8011ffcd3e1d4807712b0192641
Bhagyam-1 <bhagyampandey@gmail.com>
2025-05-05 05:13:59 +0530

    Added robots.txt and manifest.json file and also vite config for build code splitting

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,12 +1,9 @@
 import Header from "./Header";
 import Home from "../../pages/Home";
-import { motion } from "framer-motion";
-import React from "react";
-
-const About = React.lazy(() => import("../../pages/About"));
-const Projects = React.lazy(() => import("../../pages/Projects"));
-const Work = React.lazy(() => import("../../pages/Work"));
-const Contact = React.lazy(() => import("../../pages/Contact"));
+import About from "../../pages/About";
+import Projects from "../../pages/Projects";
+import Work from "../../pages/Work";
+import Contact from "../../pages/Contact";
 
 const Layout = () => {
 
@@ -14,7 +11,7 @@ const Layout = () => {
         <div className="w-screen h-screen dark:bg-dark-background flex flex-col">
             <Header />
 
-            <motion.main
+            <main
                 id="main-scroll-container"
                 role="main"
                 className="flex flex-col gap-16 items-center flex-grow text-light-text dark:text-dark-text overflow-y-auto overflow-x-hidden"
@@ -27,7 +24,7 @@ const Layout = () => {
                 <hr className="w-[96vw] dark:border-dark-shadow" />
                 <Work />
                 <Contact />
-            </motion.main>
+            </main>
         </div>
     );
 
--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,34 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-  ],
+     ],
+  build: {
+    rollupOptions: {
+        output: {
+            manualChunks(id) {
+                if (id.includes('node_modules')) {
+                    return 'vendor';
+                }
+                if (id.includes('src/common')) {
+                    return 'common-components';
+                }
+                if (id.includes('src/components/')) {
+                    return 'components';
+                }
+                if (id.includes('src/models')) {
+                    return 'models';
+                }
+                if (id.includes('src/pages')) {
+                    return 'pages';
+                }
+                if (id.includes('src/utility')) {
+                    return 'utility';
+                }
+                if (id.includes('src/assets')) {
+                    return 'assets';
+                }
+            },
+        },
+    },
+  },
 })
